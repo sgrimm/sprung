fea2100572f740eb66eeb5cd28b43fa67f5b34ab
Steven Grimm <koreth@gmail.com>
2014-03-26 10:38:33 -0700

    Add Spring test lib, Clojure component lib

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject sprung "0.1.0"
   :description "TODO"
-  :dependencies [[org.springframework/spring-core "3.2.0.RELEASE"]
-                 [org.springframework/spring-context "3.2.0.RELEASE"]]
+  :dependencies [[com.stuartsierra/component "0.2.1"]
+                 [org.springframework/spring-core "3.2.0.RELEASE"]
+                 [org.springframework/spring-context "3.2.0.RELEASE"]
+                 [org.springframework/spring-test "3.2.0.RELEASE"]] 
   :profiles {:dev {:dependencies [[junit/junit "4.8.2"]]}}
   :plugins [[lein-junit "1.1.2"]]
   :source-paths ["src/main/clojure"]
