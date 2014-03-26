99779a27ffc3cb6eaa940300d35b70b99e69bb88
Steven Grimm <koreth@gmail.com>
2014-03-26 10:56:10 -0700

    Rename test class so lein junit can find it

--- src/test/java/sprung/SpringAppTest.java
+++ src/test/java/sprung/SpringAppTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-config.xml")
-public class SpringAppTests {
+public class SpringAppTest {
     @Autowired
     private HelloService helloService;
 
